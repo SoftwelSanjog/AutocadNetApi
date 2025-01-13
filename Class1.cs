4142fc7883e06a54980a8ecf105a10c0f922308c
Sanjog Shakya <softwel.sanjog@gmail.com>
2025-01-13 17:36:31 +0545

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime; 
+using System;
+
+namespace AutocadNetApi
+{
+    public static class MyAutocad
+    {
+        [CommandMethod("tm")]
+        public static void TestMessage()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            ed.WriteMessage("Hello, AutoCAD!");
+        }
+    }
+}
+
