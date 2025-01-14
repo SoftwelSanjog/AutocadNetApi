3979ee1d58090518ecfc1cfddb24c98bd0a5d81e
Sanjog <softwel.sanjog@gmail.com>
2025-01-14 09:13:19 +0545

    #test1

--- MyAutocad.cs
+++ MyAutocad.cs
@@ -1,16 +1,16 @@
 ﻿using Autodesk.AutoCAD.ApplicationServices;
-using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.EditorInput;
-using Autodesk.AutoCAD.Runtime; 
-using System;
+using Autodesk.AutoCAD.Runtime;
 
 namespace AutocadNetApi
 {
     public static class MyAutocad
     {
-        [CommandMethod("tm")]
+        [CommandMethod("xox")]
         public static void TestMessage()
         {
+            //frmMain objMain = new frmMain();
+            //objMain.ShowDialog();
             Document doc = Application.DocumentManager.MdiActiveDocument;
             Editor ed = doc.Editor;
             ed.WriteMessage("Hello, AutoCAD!");
